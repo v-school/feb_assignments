a7b99095c950e46b1fec0c95d7ff48f748a8d3c2
V School <learn@vschool.io>
2019-03-04 13:47:13 -0700

    first commit

---
+++ exercises/data-types/app.js
@@ -0,0 +1,11 @@
+var shopper = {
+    name: "Emma",
+    age: 20,
+    hasKids: false,
+    shoppingList: ["carrots", "dog food"],
+    haveBirthday: function(){
+        
+    }
+ }
+
+ shopper.shoppingList
